40b9359ffa256dab66a52f422a9a4de07366950a
thyagostall <thstall@gmail.com>
2017-01-22 21:28:48 -0200

    Created a Service for fetching the data

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,8 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero';
+import { OnInit } from '@angular/core';
 
-const HEROES: Hero[] = [
-	{ id: 11, name: 'Mr. Nice' },
-	{ id: 12, name: 'Narco' },
-	{ id: 13, name: 'Bombasto' },
-	{ id: 14, name: 'Celeritas' },
-	{ id: 15, name: 'Magneta' },
-	{ id: 16, name: 'RubberMan' },
-	{ id: 17, name: 'Dynama' },
-	{ id: 18, name: 'Dr. IQ' },
-	{ id: 19, name: 'Magma' },
-	{ id: 20, name: 'Tornado' }
-];
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
 	styles: [`
@@ -75,16 +65,28 @@ const HEROES: Hero[] = [
 	  		</li>
 	  	</ul>
 	  	<my-hero-detail [hero]="selectedHero"></my-hero-detail>
-  	`
+  	`,
+  	providers: [HeroService]
 })
 
-export class AppComponent { 
-	heroes = HEROES;
+export class AppComponent implements OnInit { 
+	heroes: Hero[];
 	title: string = 'Tour of Heroes';
 	selectedHero: Hero;
 
+	constructor(private heroService: HeroService) {
+
+	}
 
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
+	}
+
+	getHeroes(): void {
+		this.heroes = this.heroService.getHeroes();
+	}
+
+	ngOnInit(): void {
+		this.getHeroes();
 	}
 }
