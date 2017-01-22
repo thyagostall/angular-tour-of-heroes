2113aa6cf219ec83a2b6b8b32da8ccf097815dc6
thyagostall <thstall@gmail.com>
2017-01-22 20:01:40 -0200

    Selected Hero
    - Bugfix in background color css of selected item on the list;
    - *ngIf directive for hiding the detail element when `selectedHero` is
      null;
    - [class.selected] when the selected user is equal to the list item

--- app/app.component.ts
+++ app/app.component.ts
@@ -40,7 +40,7 @@ const HEROES: Hero[] = [
 			border-radius: 4px;
 		}
 		.heroes li.selected:hover {
-			backgrounf-color: #bbd8dc !important;
+			background-color: #bbd8dc !important;
 			color: white;
 		}
 		.heroes li:hover {
@@ -72,15 +72,19 @@ const HEROES: Hero[] = [
 	  	<h1>{{title}}</h1>
 	  	<h2>My Heroes</h2>
 	  	<ul class="heroes">
-	  		<li *ngFor="let hero of heroes">
+	  		<li *ngFor="let hero of heroes" 
+				[class.selected]="hero === selectedHero"
+	  			(click)="onSelect(hero)">
 	  			<span class="badge">{{hero.id}}</span> {{hero.name}}
 	  		</li>
 	  	</ul>
-	  	<h2>{{hero.name}} details!</h2>
-	  	<div><label>id: </label> {{hero.id}}</div>
-	  	<div>
-	  		<label>name: </label> 
-	  		<input [(ngModel)]="hero.name" placeholder="name">
+	  	<div *ngIf="selectedHero">
+		  	<h2>{{selectedHero.name}} details!</h2>
+		  	<div><label>id: </label> {{selectedHero.id}}</div>
+		  	<div>
+		  		<label>name: </label> 
+		  		<input [(ngModel)]="selectedHero.name" placeholder="name">
+		  	</div>
 	  	</div>
   	`
 })
@@ -88,8 +92,10 @@ const HEROES: Hero[] = [
 export class AppComponent { 
 	heroes = HEROES;
 	title: string = 'Tour of Heroes';
-	hero: Hero = {
-		id: 1,
-		name: 'Windostorm'
-	};
+	selectedHero: Hero;
+
+
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
 }
