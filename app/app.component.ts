3ed45cf84ba7484b0228cffb6112d969a8b3120e
thyagostall <thstall@gmail.com>
2017-01-22 22:08:25 -0200

    Refactored out code from AppComponent into HeroesComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,92 +1,13 @@
 import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
 
 @Component({
-	styles: [`
-		.selected {
-			background-color: #CFD8DC !important;
-		}
-		.heroes {
-			margin: 0 0 2em 0;
-			list-style-type: none;
-			padding: 0;
-			width: 15em;
-		}
-		.heroes li {
-			cursor: pointer;
-			position: relative;
-			left: 0;
-			background-color: #eee;
-			margin: .5em;
-			padding: .3em 0;
-			height: 1.6em;
-			border-radius: 4px;
-		}
-		.heroes li.selected:hover {
-			background-color: #bbd8dc !important;
-			color: white;
-		}
-		.heroes li:hover {
-			color: #607d8b;
-			background-color: #ddd;
-			left: .1em;
-		}
-		.heroes .text {
-			position: relative;
-			top: -3px;
-		}
-		.heroes .badge {
-			display: inline-block;
-			font-size: small;
-			color: white;
-			padding: 0.8em 0.7em 0 0.7em;
-			background-color: #607d8b;
-			line-height: 1em;
-			position: relative;
-			left: -1px;
-			top: -4px;
-			height: 1.8em;
-			margin-right: .8em;
-			border-radius: 4px 0 0 4px;
-		}
-	`],
-  	selector: 'my-app',
-  	template: `
+	selector: 'my-app',
+	template: `
 	  	<h1>{{title}}</h1>
-	  	<h2>My Heroes</h2>
-	  	<ul class="heroes">
-	  		<li *ngFor="let hero of heroes" 
-				[class.selected]="hero === selectedHero"
-	  			(click)="onSelect(hero)">
-	  			<span class="badge">{{hero.id}}</span> {{hero.name}}
-	  		</li>
-	  	</ul>
-	  	<my-hero-detail [hero]="selectedHero"></my-hero-detail>
-  	`,
-  	providers: [HeroService]
+	  	<my-heroes></my-heroes>
+	  	`
 })
 
-export class AppComponent implements OnInit { 
-	heroes: Hero[];
+export class AppComponent {
 	title: string = 'Tour of Heroes';
-	selectedHero: Hero;
-
-	constructor(private heroService: HeroService) {
-
-	}
-
-	onSelect(hero: Hero): void {
-		this.selectedHero = hero;
-	}
-
-	getHeroes(): void {
-		this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-	}
-
-	ngOnInit(): void {
-		this.getHeroes();
-	}
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,8 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } 	 from '@angular/forms';
 
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { AppComponent }  		from './app.component';
+import { HeroesComponent } 		from './heroes.component';
+import { HeroDetailComponent } 	from './hero-detail.component';
+import { HeroService } 			from './hero.service';
 
 @NgModule({
 	imports: [ 
@@ -12,10 +14,15 @@ import { HeroDetailComponent } from './hero-detail.component';
 	],
   	declarations: [ 
   		AppComponent,
+  		HeroesComponent,
   		HeroDetailComponent
   	],
   	bootstrap: [ 
   		AppComponent 
+  	],
+  	providers: [
+  		HeroService
   	]
 })
+
 export class AppModule { }
