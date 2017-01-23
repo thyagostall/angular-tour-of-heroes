c8f64ca9adfbd71a7ad163389cebe9bb6cd1849a
thyagostall <thstall@gmail.com>
2017-01-22 23:32:40 -0200

    Refactored some code out of the component and added more routing

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,79 +1,25 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
-	styles: [`
-		.selected {
-			background-color: #CFD8DC !important;
-		}
-		.heroes {
-			margin: 0 0 2em 0;
-			list-style-type: none;
-			padding: 0;
-			width: 15em;
-		}
-		.heroes li {
-			cursor: pointer;
-			position: relative;
-			left: 0;
-			background-color: #eee;
-			margin: .5em;
-			padding: .3em 0;
-			height: 1.6em;
-			border-radius: 4px;
-		}
-		.heroes li.selected:hover {
-			background-color: #bbd8dc !important;
-			color: white;
-		}
-		.heroes li:hover {
-			color: #607d8b;
-			background-color: #ddd;
-			left: .1em;
-		}
-		.heroes .text {
-			position: relative;
-			top: -3px;
-		}
-		.heroes .badge {
-			display: inline-block;
-			font-size: small;
-			color: white;
-			padding: 0.8em 0.7em 0 0.7em;
-			background-color: #607d8b;
-			line-height: 1em;
-			position: relative;
-			left: -1px;
-			top: -4px;
-			height: 1.8em;
-			margin-right: .8em;
-			border-radius: 4px 0 0 4px;
-		}
-	`],
+	moduleId: module.id,
   	selector: 'my-heroes',
-  	template: `
-	  	<h2>My Heroes</h2>
-	  	<ul class="heroes">
-	  		<li *ngFor="let hero of heroes" 
-				[class.selected]="hero === selectedHero"
-	  			(click)="onSelect(hero)">
-	  			<span class="badge">{{hero.id}}</span> {{hero.name}}
-	  		</li>
-	  	</ul>
-	  	<my-hero-detail></my-hero-detail>
-  	`
+  	templateUrl: 'heroes.component.html',
+  	styleUrls: [ 'heroes.component.css' ]
 })
 
 export class HeroesComponent implements OnInit { 
 	heroes: Hero[];
 	selectedHero: Hero;
 
-	constructor(private heroService: HeroService) {
-
-	}
+	constructor(
+		private heroService: HeroService,
+		private router: Router
+	) {}
 
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
@@ -81,6 +27,10 @@ export class HeroesComponent implements OnInit {
 
 	getHeroes(): void {
 		this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+	}
+
+	gotoDetail(): void {
+		this.router.navigate(['/detail', this.selectedHero.id]);
 	}
 
 	ngOnInit(): void {
