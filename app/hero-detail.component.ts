c3f077302be2d74c39c9131aa44bb07d76a2a215
thyagostall <thstall@gmail.com>
2017-01-24 23:13:10 -0200

    Bugfix for single hero; Updating Hero

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -27,6 +27,11 @@ export class HeroDetailComponent implements OnInit {
 		this.location.back();
 	}	
 
+	save(): void {
+		this.heroService.update(this.hero)
+			.then(() => this.goBack());
+	}	
+
 	ngOnInit(): void {
 		this.route.params
 			.switchMap((params: Params) => this.heroService.getHero(+params['id']))
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -7,6 +7,7 @@ import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
+	private headers = new Headers({'Content-Type': 'application/json'});
 	private heroesUrl = 'api/heroes';
 
 	constructor(private http: Http) {}
@@ -19,11 +20,20 @@ export class HeroService {
 	}
 
 	getHero(id: number): Promise<Hero> {
-		const url = '${this.heroesUrl}/${id}';
+		const url = `${this.heroesUrl}/${id}`;
 		return this.http.get(url)
 					.toPromise()
 					.then(response => response.json().data as Hero)
 					.catch(this.handleError);	
+	}
+
+	update(hero: Hero): Promise<Hero> {
+		const url = `${this.heroesUrl}/${hero.id}`;
+		return this.http
+				.put(url, JSON.stringify(hero), {headers: this.headers})
+				.toPromise()
+				.then(() => hero)
+				.catch(this.handleError);
 	}
 
 	private handleError(error: any): Promise<any> {
