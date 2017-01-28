dec27a5aa5001dbf7dc48558f6220a5259cc243a
thyagostall <thstall@gmail.com>
2017-01-28 19:42:08 -0200

    Search Service and Components

--- app/app.module.ts
+++ app/app.module.ts
@@ -11,8 +11,11 @@ import { DashboardComponent }	from './dashboard.component';
 import { HeroesComponent } 		from './heroes.component';
 import { HeroDetailComponent } 	from './hero-detail.component';
 import { HeroService } 			from './hero.service';
+import { HeroSearchComponent }       from './hero-search.component';
 
 import { AppRoutingModule }	from './app-routing.module'
+
+import './rxjs-extensions';
 
 @NgModule({
     imports: [ 
@@ -26,7 +29,8 @@ import { AppRoutingModule }	from './app-routing.module'
     		AppComponent,
     		DashboardComponent,
     		HeroesComponent,
-    		HeroDetailComponent
+    		HeroDetailComponent,
+        HeroSearchComponent
   	],
   	bootstrap: [ 
   		  AppComponent 
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -45,7 +45,7 @@ export class HeroService {
 
 	}
 
-	delete(id: number): Promise<Void> {
+	delete(id: number): Promise<void> {
 		const url = `${this.heroesUrl}/${id}`;
 		return this.http.delete(url, {headers: this.headers})
 					.toPromise()
